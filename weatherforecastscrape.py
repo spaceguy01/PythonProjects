75b0641d1d9fdaa04bf8de75fc1a591e52b368f1
spaceguy01 <joonyi@gmail.com>
2018-05-03 17:23:15 -0700

    1,3-5,7-10 day weather forecast

--- weatherforecastscrape.py
+++ weatherforecastscrape.py
@@ -1,19 +1,26 @@
 from selenium import webdriver
 driver = webdriver.Chrome()
 
+"""Input a City to get the 1, 3-5, 7-10 day weather forecast"
+
 city = str(input("Enter the name of the city: ")).replace(' ','-')
 
 driver.get('https://www.weather-forecast.com/locations/'+city+"/forecasts/latest")
 
 
 print('-'*60)
+
 print (driver.find_elements_by_class_name("b-forecast__table-description-title")[0].text)
 print (driver.find_elements_by_class_name("b-forecast__table-description-content")[0].text)
+
 print ('-'*60)
 
 print (driver.find_elements_by_class_name("b-forecast__table-description-title")[1].text)
 print (driver.find_elements_by_class_name("b-forecast__table-description-content")[1].text)
+
 print ('-'*60)
+
 print (driver.find_elements_by_class_name("b-forecast__table-description-title")[2].text)
 print (driver.find_elements_by_class_name("b-forecast__table-description-content")[2].text)
+
 print ('-'*60)
